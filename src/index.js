a1445b48f151f1c2735a7e536258711e9407f3b6
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-12-16 16:30:58 +0300

    feat: add API-client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-console.log('It works!')
+class SwapiService {
+
+  _apiBase = 'https://www.swapi.tech/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  async getPerson(id) {
+    const res = await this.getResource(`/people/${id}`);
+    return res.result.properties;
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  async getPlanet(id) {
+    const res = await this.getResource(`/planets/${id}`);
+    return res.result.properties;
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  async getStarship(id) {
+    const res = await this.getResource(`/starships/${id}`);
+    return res.result.properties;
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getStarship(3).then((p) => {
+    console.log(p.name);
+});
+
+swapi.getAllStarships().then((planets) => {
+  planets.forEach((planet) => {
+    console.log(planet.name)
+  });
+});

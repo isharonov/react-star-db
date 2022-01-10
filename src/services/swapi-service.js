e7519d0a2f7218284286a998aa70226d41c937b3
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-10 12:12:35 +0300

    feat: add basic components layout

---
+++ src/services/swapi-service.js
@@ -0,0 +1,44 @@
+export default class SwapiService {
+
+  _apiBase = 'https://www.swapi.tech/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  async getPerson(id) {
+    const res = await this.getResource(`/people/${id}`);
+    return res.result.properties;
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  async getPlanet(id) {
+    const res = await this.getResource(`/planets/${id}`);
+    return res.result.properties;
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  async getStarship(id) {
+    const res = await this.getResource(`/starships/${id}`);
+    return res.result.properties;
+  }
+}

bd1523cc23cd48509233402e91f3404521d34491
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-11 13:25:09 +0300

    7-66: Use in practice componentDidMount()

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,7 +17,8 @@ export default class RandomPlanet extends Component {
 
   componentDidMount() {
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 10000);
+    // Disable auto update, because server returns error 429 (Too Many Requests)
+    //this.interval = setInterval(this.updatePlanet, 10000);
   }
 
   onPlanetLoaded = (planet) => {

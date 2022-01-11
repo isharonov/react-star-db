c382a1ace4a608e32fe8e2170a4cdca0e322732a
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-11 18:43:35 +0300

    8-74: Render functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -60,7 +60,10 @@ export default class App extends Component {
           <div className="col-md-6">
             <ItemList
               onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets} />
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => (
+                <span>{item.name} <button>!</button></span>
+              )}/>
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
@@ -71,7 +74,9 @@ export default class App extends Component {
           <div className="col-md-6">
             <ItemList
               onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships} />
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
+            />
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -38,6 +38,7 @@ export default class PeoplePage extends Component {
           <ItemList
             onItemSelected={this.onPersonSelected}
             getData={this.swapiService.getAllPeople}
+            renderItem={(item) => item.name}
           />
         </div>
         <div className="col-md-6">

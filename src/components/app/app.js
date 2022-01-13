e993d0481ea6a02a64c91f45eb19c0c1c3e3de62
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-13 11:41:45 +0300

    8-84: Using Context API

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
 import Header from "../header";
-
+import { SwapiServiceProvider } from "../swapi-service-context";
 import './app.css';
 import SwapiService from "../../services/swapi-service";
 import ErrorBoundry from "../error-boundry";
@@ -32,22 +32,24 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
+        <SwapiServiceProvider value={this.swapiService} >
+          <div className="stardb-app">
+            <Header />
 
-          <PersonDetails itemId={11} />
+            <PersonDetails itemId={11} />
 
-          <PlanetDetails itemId={5} />
+            <PlanetDetails itemId={5} />
 
-          <StarshipDetails itemId={5} />
+            <StarshipDetails itemId={5} />
 
-          <PersonList />
+            <PersonList />
 
-          <StarshipList />
+            <StarshipList />
 
-          <PlanetList />
+            <PlanetList />
 
-        </div>
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   };

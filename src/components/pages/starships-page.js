4c354f5ba163cc9e1ed05cdc7ce458b5e9bb52e5
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-17 22:44:15 +0300

    10-110: withRouter -> useNavigate

--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -1,24 +1,15 @@
-import React, {Component} from "react";
-import {StarshipDetails, StarshipList} from "../sw-components";
-import Row from "../Row";
+import React from "react";
+import {StarshipList} from "../sw-components";
+import { useNavigate } from "react-router-dom";
 
-export default class StarshipsPage extends Component {
+const StarshipsPage = () => {
+  let navigate = useNavigate();
+  return (
+    <StarshipList
+      onItemSelected={ (itemId) => {
+        navigate(`/starships/${itemId}`);
+      }}/>
+  );
+};
 
-  state = {
-    selectedItem: null
-  }
-
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  }
-
-  render() {
-    const { selectedItem } = this.state;
-
-    return (
-      <Row
-        left={<StarshipList onItemSelected={this.onItemSelected} />}
-        right={<StarshipDetails itemId={selectedItem} />} />
-    );
-  }
-}
+export default StarshipsPage;

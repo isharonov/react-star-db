55e0d8da1950075aa93bd774188ad1b93528fc4e
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-17 18:55:03 +0300

    10-108: How Route works

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -37,6 +37,7 @@ export default class App extends Component {
               <RandomPlanet />
 
               <Routes>
+                <Route path="/" element={<h2>Welcome to Star DB</h2>} />
                 <Route path="/people" element={<PeoplePage />} />
                 <Route path="/planets" element={<PlanetsPage />} />
                 <Route path="/starships" element={<StarshipsPage />} />
